95c9b21772a89adc1cf067558564184edb8478ff
killertunes <edc@wwjjd.net>
2023-01-01 22:54:23 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+<h2 style="text-align: center;"><img class="alignnone wp-image-52" src="https://wwjjd.net/wp-content/uploads/2021/10/mmj-owl-300x116.png" alt="" width="437" height="169" /></h2>
+<h2 style="text-align: center;">The largest curated live performance archive on the net</h2>
+<p style="text-align: center;">Please visit these links for <a href="http://www.jimjames.com" target="_blank" rel="noopener"><em>Jim James</em></a> &amp;
+<a href="http://www.mymorningjacket.com" target="_blank" rel="noopener"><em>My Morning Jacket's</em></a> official pages.
+They both have stores &amp; links to major outlets where you can purchase most of the music heard during the these archived
+shows. Attending live shows is also <a href="https://rumble.com/vx6s81-my-morning-jacket-one-big-holiday-3-aftermovie.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener"><em>highly</em></a>
+recommended &amp; the best way to show your support.</p>
+
+<h2 style="text-align: center;">2021</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6wjx-my-morning-jacket-forest-hills-part-1-9112021.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Forest Hills Stadium 09/11/2021 - Part 1</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6xa1-my-morning-jacket-forest-hills-part-2-9112021.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Forest Hills Stadium 09/11/2021 - Part 2</a></p>
+
+<h2 style="text-align: center;">2019</h2>
+<p style="text-align: center;">Red Rocks Ampitheatre 08/2-3/2019</p>
+
+<h2 style="text-align: center;">2018</h2>
+<p style="text-align: center;"><a href="https://rumble.com/search/video?q=my%20morning%20jacket%20obh4" target="_blank" rel="noopener">One Big Holiday 4 03/2-5/2018</a></p>
+
+<h2 style="text-align: center;">2017</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx2l55-my-morning-jacket-1st-bank-center-12312017.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">1stBank Center 12/31/2017</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6k9d-my-morning-jacket-1st-bank-center-12302017.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">1stBank Center 12/30/2017</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6j2v-my-morning-jacket-1st-bank-center-12292017.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">1stBank Center 12/29/2017</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/search/video?q=my%20morning%20jacket%20peach%20festival" target="_blank" rel="noopener">Peach Music Festival 08/11/2017</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx2n2v-my-morning-jacket-forest-hills-stadium-7152017.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Forest Hills Stadium 07/15/2017</a></p>
+<p style="text-align: center;"><a href="https://youtu.be/UTVhSEHa6Ow" target="_blank" rel="noopener">Charlotte Metro Credit Union Ampitheatre 07/08/2017</a></p>
+<p style="text-align: center;"><a href="https://youtu.be/taorA_tP-48" target="_blank" rel="noopener">PNC Pavilion at Riverbend 06/22/2017</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/search/video?q=my%20morning%20jacket%20obh3" target="_blank" rel="noopener">One Big Holiday 3 02/4-7/2017</a></p>
+
+<h2 style="text-align: center;">2016</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx2nsz-my-morning-jacket-lockn-festival-8272016.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Lockn' Festival 08/27/2016</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6m87-my-morning-jacket-red-rocks-amphitheatre-5292016.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Red Rocks Ampitheatre 05/29/2016</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6lq5-my-morning-jacket-red-rocks-amphitheatre-5282016.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Red Rocks Ampitheatre 05/28/2016</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6u89-my-morning-jacket-acl-live-at-the-moody-theater-522016.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Austin City Limits 05/02/2016</a></p>
+
+<h2 style="text-align: center;">2015</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx1ryl-my-morning-jacket-lollapalooza-berlin-9132015.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Lollapalooza 09/13/2015</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vwz847-my-morning-jacket-red-rocks-8142015.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Red Rocks Ampitheatre 08/14/2015</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx2jtd-my-morning-jacket-bonnaroo-6132015.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Bonnaroo 06/13/2015</a></p>
+<p style="text-align: center;"><a href="https://youtu.be/tq36F5DiOes" target="_blank" rel="noopener">The Chicago Theatre 06/11/2015</a></p>
+
+<h2 style="text-align: center;">2013</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx2kh9-my-morning-jacket-bridge-school-benefit-10262013.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Bridge School Benefit 10/26/2013</a></p>
+
+<h2 style="text-align: center;">2012</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6itf-my-morning-jacket-you-who-982012.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">You Who 09/08/2012</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx2akb-my-morning-jacket-red-rocks-amphitheatre-842012.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Red Rocks Ampitheatre 08/04/2012</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx2k65-my-morning-jacket-forecastle-festival-7142012.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Forecastle Festival 07/14/2012</a></p>
+
+<h2 style="text-align: center;">2011</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx2i35-my-morning-jacket-red-rocks-amphitheatre-842011.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Red Rocks Ampitheatre 08/04/2011</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6krz-my-morning-jacket-village-recording-studios-6212011.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Village Recording Studios 06/21/2011</a></p>
+<p style="text-align: center;"><a href="https://youtu.be/zB68InBarTM" target="_blank" rel="noopener">Mountain Jam 06/05/2011</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6msh-my-morning-jacket-vh1-storytellers-632011.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">VH1 Storytellers 06/03/2011</a></p>
+
+<h2 style="text-align: center;">2008</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx2nej-my-morning-jacket-luxor-722008.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Luxor Cologne, Germany 07/02/2008</a></p>
+<p style="text-align: center;"><a href="https://youtu.be/7R71shGsw3Y" target="_blank" rel="noopener">Desmet, Amsterdam, Netherlands 05/16/2008</a></p>
+
+<h2 style="text-align: center;">2006</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6im7-my-morning-jacket-bonnaroo-6162006.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Bonnaroo 06/16/2006</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx2mof-my-morning-jacket-austin-city-limits-2006.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Austin City Limits 2006</a></p>
+
+<h2 style="text-align: center;">2005</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6l97-my-morning-jacket-okonokos-11122005.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Okonokos The Fillmore 11/11-12/2005</a></p>
+
+<h2 style="text-align: center;">2004</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx2kpl-my-morning-jacket-the-ballroom-at-gypsy-tea-room-9172004.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">The Ballroom at Gypsy Tea Room 09/17/2004</a></p>
+
+<h2 style="text-align: center;">2003</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6iep-my-morning-jacket-mercury-lounge-part-2-632003.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Mercury Lounge Set 2 06/03/2003</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx4nzv-my-morning-jacket-mercury-lounge-part-1-632003.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Mercury Lounge Set 1 06/03/2003</a></p>
+<p style="text-align: center;"><a href="https://rumble.com/vx6kjn-my-morning-jacket-exitin-4232003.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Exit/In 04/23/2003</a></p>
+
+<h2 style="text-align: center;">2002</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6ip9-my-morning-jacket-the-earl-3212002.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">The Earl 03/21/2002</a></p>
+
+<h2 style="text-align: center;">2001</h2>
+<p style="text-align: center;"><a href="https://rumble.com/vx6kf7-my-morning-jacket-mercury-lounge-10252001.html?mref=wd1co&amp;mc=ekjqs" target="_blank" rel="noopener">Mercury Lounge 10/25/2001</a></p>
+
+<h2 style="text-align: center;">1999</h2>
+<p style="text-align: center;">Headliners 09/16/1999</p>
+
+<div class="col" style="text-align: center;"></div>
